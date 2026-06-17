43efac3fb288032be68175e005aded6f20f7281b
Soodabeh Bahrekazemi <bahrekazemi@gmail.com>
2026-06-17 12:30:51 +0200

    requirements and setup are updated

---
+++ setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import find_packages
+from setuptools import setup
+
+requirements = []
+
+if os.path.isfile('requirements.txt'):
+    with open('requirements.txt') as f:
+        content = f.readlines()
+    requirements.extend([x.strip() for x in content if 'git+' not in x])
+
+
+setup(name='RETENTION-AGENT',
+      version="0.0.1",
+      description="Customer churn prediction API",
+      packages=find_packages(),
+      install_requires=requirements,
+      test_suite='tests',
+      # include_package_data: to install data from MANIFEST.in
+      include_package_data=True,
+      # scripts=['scripts/packagename-run'],
+      zip_safe=False)
